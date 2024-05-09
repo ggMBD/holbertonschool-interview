9985a18a62c410eeeb99ef24da9d6bea0656bc3c
maouia <maouiabend@gmail.com>
2024-05-09 21:19:08 +0100

    0. Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes in the given list can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes. Each inner list contains the keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    visited = set()
+    visited.add(0)
+    stack = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and key not in visited:
+                stack.append(key)
+                visited.add(key)
+    return len(visited) == len(boxes)
